55e3ed5290959767c8a0502757f5ebddff70a85f
morpheus <mariageorgepapas@gmail.com>
2020-01-02 06:26:54 -0600

    Added basic html for "tracking/add" template, code for controller and model doesn't work.

--- src/main/java/com/wecanbetethis/diabetesapp/Controllers/BloodSugarController.java
+++ src/main/java/com/wecanbetethis/diabetesapp/Controllers/BloodSugarController.java
@@ -1,14 +1,31 @@
 package com.wecanbetethis.diabetesapp.Controllers;
 
+import com.wecanbetethis.diabetesapp.Models.BloodSugar;
 import com.wecanbetethis.diabetesapp.Models.Data.BloodSugarDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping
+@RequestMapping("tracking")
 public class BloodSugarController {
 
     @Autowired
     private BloodSugarDao bloodSugarDao;
+
+    @RequestMapping(value= "", method = RequestMethod.GET)
+    public String addForm(Model model) {
+        return "tracking/add";
+    }
+
+//    @RequestMapping(value = "/tracking", method = RequestMethod.GET)
+//    public String addForm(Model model) {
+//        BloodSugar bloodSugar = new BloodSugar();
+//        model.addAttribute("bloodsugar", bloodSugar);
+//        model.addAttribute("title", "Blood Sugar Entry");
+//        return "tracking/add";
+//    }
+
 }
--- src/main/java/com/wecanbetethis/diabetesapp/Models/BloodSugar.java
+++ src/main/java/com/wecanbetethis/diabetesapp/Models/BloodSugar.java
@@ -11,4 +11,34 @@ public class BloodSugar {
     @GeneratedValue
     private int id;
 
+//    public enum Entry {
+//        FASTING, BREAKFAST, MORNING_SNACK, LUNCH, AFTERNOON_SNACK, DINNER, EVENING_SNACK
+//    }
+//
+//    String Entry;
+//    Integer bloodSugarEntry;
+//
+//    public BloodSugar() {
+//    }
+//
+//    public BloodSugar(String entry, Integer bloodSugarEntry) {
+//        Entry = entry;
+//        this.bloodSugarEntry = bloodSugarEntry;
+//    }
+//
+//    public String getEntry() {
+//        return Entry;
+//    }
+//
+//    public void setEntry(String entry) {
+//        Entry = entry;
+//    }
+//
+//    public Integer getBloodSugarEntry() {
+//        return bloodSugarEntry;
+//    }
+//
+//    public void setBloodSugarEntry(Integer bloodSugarEntry) {
+//        this.bloodSugarEntry = bloodSugarEntry;
+//    }
 }
